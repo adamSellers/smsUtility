98b3d5ca7e88179eb7f749c7ebb3661ed1533427
Adam Sellers <adza007@gmail.com>
2017-11-16 13:46:50 +1100

    testing finished

---
+++ mdapiout/aura/SMSGun/SMSGunController.js
@@ -0,0 +1,67 @@
+({
+    doInit: function (component, event, helper) {
+
+        //run two init functions, get mobile number and user ID
+        helper.initMobileNumber(component, event);
+
+        helper.initUserId(component, event);
+        
+    },
+
+    sendSMS: function (component, event, helper) {
+
+        //grab the message text first
+        var messageText = component.get('v.messageBody');
+
+        var action = component.get("c.sendMessage");
+
+        action.setParams({
+            mobileNumber: component.get("v.mobileNumber"),
+            messageText: messageText,
+            mcKeyword: component.get("v.mcKeyword"),
+            mcApikey: component.get("v.mcApiKey"),
+            mcClientId: component.get("v.mcClientId"),
+            mcClientSecret: component.get("v.mcClientSecret")
+        });
+
+        action.setCallback(this, function (response) {
+
+            var state = response.getState();
+
+            if (state == "SUCCESS") {
+                
+                var tokenId = response.getReturnValue();
+
+                console.log('success! Token Id is: ' + JSON.stringify(tokenId));
+                
+                var toastEvent = $A.get("e.force:showToast");
+                toastEvent.setParams({
+                    title: "Success!",
+                    type : "success",
+                    message: "SMS Id: " + tokenId + " has been delivered successfully."
+                });
+                toastEvent.fire();
+                //log a call with the details
+                helper.logCall(component, event, tokenId, messageText);
+
+                //clear the form to send next message
+                component.set('v.messageBody', null);
+                
+            } else if (state == "ERROR") {
+                var errors = response.getError();
+                if (errors) {
+                    if (errors[0] && errors[0].message) {
+                        console.log("Error message: " +
+                            errors[0].message);
+                    }
+                } else {
+                    console.log("Unknown error");
+                }
+            }
+
+        });
+
+        $A.enqueueAction(action);
+    }
+
+})
